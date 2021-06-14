7324af1e5a6281f902fa8e1867a3ee8fd06517c7
AnhTuanIT <ngocdungbb1@gmail.com>
2021-06-14 20:15:10 +0700

    test conflict

--- func.js
+++ func.js
@@ -1,7 +1,7 @@
 const reverseText = (str) => {
     return str.split('').reverse().join('');
 }
-console.log(reverseText('Tuan'));
+console.log(reverseText('Toi la Tuan'));
 
 console.log(reverseText('Dat'));
 // sdfasdfasfsdf 7:57PM
